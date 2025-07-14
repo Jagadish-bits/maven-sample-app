d4ac7991ec7be08b4da0475999cb5abe406adeb2
jagadish-bits <2021wb86627@wilp.bits-pilani.ac.in>
2025-07-14 18:22:59 +0530

    Fixed unclosed string literal in App.java

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Task 4 - Jenkins CI Trigger Test!
-" );
+        System.out.println( "This is Task 4 - Jenkins CI Trigger Test!" );
     }
 }
